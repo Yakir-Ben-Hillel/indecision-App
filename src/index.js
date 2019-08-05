557ad8979b1c8225491d60ac6193dcd275f9c80e
Yakir Ben Hillel <yakir985@gmail.com>
2019-08-05 22:00:59 +0300

    Learnt about classes and components

--- src/index.js
+++ src/index.js
@@ -1,54 +1,72 @@
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable no-unused-vars */
+/* eslint-disable require-jsdoc */
 import React from 'react';
 import ReactDOM from 'react-dom';
-console.log('App.js is running!');
+// SImple options
+class IndecisionApp extends React.Component {
+  render() {
+    const title = 'Indecision App';
+    const subTitle = 'Put your life in the hands of a computer';
+    const options = ['Option one', 'Option two', 'Option three'];
 
-const app = {
-	title: 'Indecision App',
-	subtitle: 'Put your life in the hands of a computer',
-	options: []
-};
-const randomNum = Math.floor(Math.random() * app.options.length);
-const onFormSubmit = e => {
-	e.preventDefault();
-	const given = e.target.elements.option.value;
-	if (given) {
-		app.options.push(given);
-		e.target.elements.option.value = null;
-	}
-	render();
-};
-const PickAnOption = () => {
-	alert(app.options[randomNum]);
-};
-const WipeArray = () => {
-	app.options = [];
-	render();
-};
-const appRoot = document.getElementById('root');
-const render = () => {
-	const template = (
-		<div>
-			<h1>{app.title}</h1>
-			{app.subtitle && <p>{app.subtitle}</p>}
-			<ol>
-				<button disabled={app.options.length === 0} onClick={PickAnOption}>
-					What should i do?
-				</button>
-				<button disabled={app.options.length === 0} onClick={WipeArray}>
-					Wipe All
-				</button>
-				<p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-				{app.options.map(string => {
-					return <p key={string}>{string}</p>;
-				})}
-			</ol>
-			<form onSubmit={onFormSubmit}>
-				<input type="text" name="option" />
-				<button>Submit</button>
-			</form>
-		</div>
-	);
-	ReactDOM.render(template, appRoot);
-};
-render();
+    return (
+      <div>
+        <Header title={title} subTitle={subTitle} />
+        <Action />
+        <Options options={options} />
+        <AddOption />
+      </div>
+    );
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subTitle}</h2>
+      </div>
+    );
+  }
+}
+
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>What should I do?</button>
+      </div>
+    );
+  }
+}
+
+class Options extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.options.map((option) => (
+          <Option key={option} option={option} />
+        ))}
+      </div>
+    );
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return (
+      <div>
+        <text>{this.props.option}</text>
+      </div>
+    );
+  }
+}
+
+class AddOption extends React.Component {
+  render() {
+    return <div>AddOption component here</div>;
+  }
+}
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('root'));

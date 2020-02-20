c7e27a5f2d9b3b7e912e54932ce32b14ca2c1519
Yakir Ben Hillel <yakir985@gmail.com>
2020-02-20 21:16:06 +0200

    made changes to the header.

--- src/components/logos.tsx
+++ src/components/logos.tsx
@@ -2,30 +2,11 @@
 /* eslint-disable arrow-parens */
 import React from 'react';
 import githubLogo from '../pictures/github-logo.png';
-import instagramLogo from '../pictures/instagram-logo.png';
-import facebookLogo from '../pictures/facebook-logo.png';
 import linkedInLogo from '../pictures/linkedin-icon.png';
 
 const Logos = () => {
   return (
     <div className='logos'>
-      <div id='facebook'>
-        {' '}
-        <a
-          href='https://www.facebook.com/yakirbenhillel'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <img
-            src={facebookLogo}
-            width='47'
-            height='47'
-            alt='  '
-            title='Facebook account'
-          />
-        </a>
-      </div>
-
       <div id='github'>
         {' '}
         <a
@@ -56,22 +37,6 @@ const Logos = () => {
             height='39'
             alt='  '
             title='linkedIn account'
-          />
-        </a>
-      </div>
-
-      <div id='instagram'>
-        <a
-          href='https://instagram.com/yakir_ben_hillel'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <img
-            src={instagramLogo}
-            width='46'
-            height='46'
-            alt='   '
-            title='Instagram account'
           />
         </a>
       </div>
